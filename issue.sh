fa64c17da6c24b1eb2e0f9a81ee1ab17040ce4e5
brianswisher <bswisher@gmail.com>
2015-04-14 16:40:53 -0700

    issue template

---
+++ issue.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# bs_htx_fix; 55555
+
+
+issue() {
+  IN="`pbpaste`"
+  arrIN=(${IN//;/ })
+
+  echo "[RAILS-${arrIN[1]}](https://hoteltonight.atlassian.net/browse/RAILS-${arrIN[1]}) - ${arrIN[0]}
+
+\`\`\`
+${arrIN[0]}
+
+RAILS-${arrIN[1]}
+
+closes #----
+\`\`\`
+
+\`bs_branch_name_RAILS_${arrIN[1]}\`
+
+\`\`\`bash
+# echo "bs_branch_name_RAILS_${arrIN[1]}" | pbcopy && curl -s https://raw.githubusercontent.com/brianswisher/shipit/master/shipit.sh | bash
+\`\`\`
+
+\`\`\`
+curl -s https://gist.githubusercontent.com/brianswisher/9a550a3d53f71e924d05/raw/cf74c7c208a7dcde5f3881c471ee25c463a54f4f/pr-checklist.sh | bash
+\`\`\`
+" | pbcopy
+
+  echo "`pbpaste`"
+}
+
+issue
